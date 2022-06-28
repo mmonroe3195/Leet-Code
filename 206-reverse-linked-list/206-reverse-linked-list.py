710c2f242d036aee6df5bb96543f037a70e86897
Madison Monroe <67032501+mmonroe3195@users.noreply.github.com>
2022-06-27 22:11:51 -0400

    Time: 57 ms (5.70%), Space: 18.7 MB (15.04%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -5,15 +5,15 @@
 #         self.next = next
 class Solution(object):      
     def reverseHelper(self, head, prev):
-            
-            if not head.next:
-                head.next = prev
-                return head
-            
             if head.next:
                 first =  self.reverseHelper(head.next, head)
                 head.next = prev
                 return first
+            
+            else:
+                head.next = prev
+                return head
+            
 
             
     def reverseList(self, head):
