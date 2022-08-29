f766b5921d04121629cb765dce4f3484e90aae1c
Madison Monroe <67032501+mmonroe3195@users.noreply.github.com>
2022-08-28 22:26:07 -0400

    Time: 6 ms (89.70%), Space: 14.8 MB (42.47%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    /* Just a quickly created solution to the problem
+    will clean up code and comment out later */ 
+    
+    void mergeHelper(ListNode* list1, ListNode* list2, ListNode* curr) {
+        
+        if (!list1 && !list2)
+            return;
+        
+        if (!list1) {
+            curr->next = list2;
+            return;
+        }
+            
+        if (!list2){
+            curr->next = list1;
+            return;
+        }
+        
+        if (list1->val < list2->val) {
+            ListNode* temp = list1->next;
+            curr->next =  list1;
+            curr = curr->next;
+            mergeHelper(temp, list2, curr);
+        }
+        
+        else {
+            ListNode* temp = list2->next;
+            curr->next = list2;
+            curr = curr->next;
+            mergeHelper(list1, temp, curr);
+        }
+        
+    }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+        ListNode* head = nullptr;
+        
+        if (!list1 && !list2)
+            return nullptr;
+        
+        if (!list1)
+            return list2;
+            
+        if (!list2)
+            return list1;
+        
+        if (list1->val < list2->val) {
+            head = list1;
+            mergeHelper(list1->next, list2, head);
+        }
+        
+        else {
+            head = list2;
+            mergeHelper(list1, list2->next, head);
+        }
+        
+        return head;
+    }
+    
+    
+};
