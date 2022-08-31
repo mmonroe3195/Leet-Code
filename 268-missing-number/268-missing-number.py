01de26ad9fde7f1f09f35cf64d9c1abf029ac4bc
Madison Monroe <67032501+mmonroe3195@users.noreply.github.com>
2022-08-31 09:08:24 -0400

    Time: 174 ms (54.26%), Space: 14.6 MB (67.49%) - LeetHub

---
+++ 268-missing-number/268-missing-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # would be easy to do in O(nlog(n))
+        # sort list -> iterate through to see missing number range
+        # this way is more efficent though
+        return sum(range(len(nums) + 1)) - sum(nums)
+        
