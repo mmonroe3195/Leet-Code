2270aa46a9b40ee238899541220de665795f0fc1
Madison Monroe <67032501+mmonroe3195@users.noreply.github.com>
2023-08-26 15:25:23 -0400

    Time: 179 ms (82.19%), Space: 14.4 MB (73.03%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,41 +1,22 @@
 import math
 
 class Solution(object):
-    
-    #method one: using no helper method
-    """def search(self, nums, target):
-        if len(nums) == 0:
+   import math
+
+class Solution(object):
+   def searchHelper(self, nums, target, start, stop):
+        if start > stop and nums[0] != target:
             return -1
-        
-        mid = int(math.floor((len(nums) - 1) / 2))
+            
+        mid = int(math.floor((start + stop) / 2))
         if nums[mid] == target:
             return mid
         
         elif target < nums[mid]:
-            return self.search(nums[:mid], target)
+            return self.searchHelper(nums, target, start, mid - 1)
         
-        right = self.search(nums[mid + 1:], target)
-        
-        if right == -1:
-            return -1
-        
-        return mid + 1 + right"""
+        return self.searchHelper(nums, target, mid + 1, stop)
+
+   def search(self, nums, target):
+        return self.searchHelper(nums, target, 0, len(nums) - 1)
     
-    #method two: using a helper function
-    def searchHelper(self, nums, start, end, target):
-        if start > end:
-            return -1
-            
-        mid = (start + end) // 2
-           
-        if target == nums[mid]:
-            return mid
-            
-        elif target < nums[mid]:
-            return self.searchHelper(nums, start, mid - 1, target)
-            
-        else:
-            return self.searchHelper(nums, mid + 1, end, target)
-            
-    def search(self, nums, target):
-        return self.searchHelper(nums, 0, len(nums) - 1, target)
