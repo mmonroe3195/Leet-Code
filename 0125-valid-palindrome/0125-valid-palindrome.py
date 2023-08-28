26ce93cecc9114e91b5b86595e2493b641b6beb7
Madison Monroe <67032501+mmonroe3195@users.noreply.github.com>
2023-08-27 20:07:13 -0400

    Time: 21 ms (95.13%), Space: 15.8 MB (20.10%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -9,7 +9,6 @@ class Solution(object):
         
         #removes all whitespace and non-alphanumeric chars
         s = re.sub(r'[\W_]', '', s) 
-        print(s)
         
         for i in range(len(s) // 2):
             if s[i] != s[len(s) - 1 - i]:
