caca252a629241e96e9b994d4bf2cc9c475a4fc0
Madison Monroe <67032501+mmonroe3195@users.noreply.github.com>
2023-09-05 16:18:00 -0400

    Time: 670 ms (5.03%), Space: 23 MB (92.96%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,12 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        
+        # O(n) time complexity and O(n) space complexity
+        """
         dict = {}
         for i in nums:
-            if dict.get(i):
-                return True
+             if dict.get(i):
+                 return True
             
-            else:
-                dict[i] = 1
+             dict[i] = 1"""
         
-        return False
+        # O(nlogn) time complexity and O(1) space complexity
+        nums.sort()
+        print(nums)
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+        return False      
+        
+            
+            
