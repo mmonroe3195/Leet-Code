ad2af0bf4a1a715ec73498da9affe76abbd0e206
Madison Monroe <67032501+mmonroe3195@users.noreply.github.com>
2023-09-05 20:46:07 -0400

    Time: 138 ms (13.05%), Space: 26.7 MB (5.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,27 @@
+from collections import Counter
 class Solution(object):
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        dict = defaultdict(list)
-        for str in strs:
-            count = [0] * 26
-            for char in str:
-                count[ord(char) - ord('a')] += 1
+        counters = []
+        dict = {}
+        # loop through each word in the list and create a counter for each word
+        # this counter will be used to keep track of anagrams
+        for word in strs:
+            counter = Counter()
+            for j in word:
+                counter[str(j)] += 1
             
-            dict[tuple(count)].append(str)
+            # use each counter as a key for a dictionary
+            # each word with the same counter is an anagram so the word should be                   appended as a value for the key
+            
+            key = tuple(sorted(counter.items()))
+            if dict.get(key):
+                dict[key].append(str(word))
+            else:
+                dict[key] = [str(word)]
                 
-            
         return dict.values()
+            
